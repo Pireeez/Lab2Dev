70baa73d6ede3467de605ee571ea0056ac2dfe80
Pireeez <gustavo.pires@lab2dev.com>
2025-07-08 20:36:08 -0300

    aba pesquisar

---
+++ Blog do Café/script/script.js
@@ -0,0 +1,19 @@
+
+function aparecerPesquisa(){
+    let nav = document.querySelector(".container-nav");
+    let nav2 = document.querySelector(".container-nav-2");
+    nav.style.display = "none";
+    nav2.style.display = "flex";
+}
+
+function sumirPesquisa(){
+    let nav = document.querySelector(".nav-box");
+    let nav3 = document.querySelector(".container-nav");
+    let nav2 = document.querySelector(".container-nav-2");
+    nav3.style.display = "block"
+    nav.style.display = "flex";
+    nav2.style.display = "none";
+}
+
+
+
